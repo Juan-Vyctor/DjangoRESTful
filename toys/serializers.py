e09767ae872be7d4769b794961072704aa40f7d1
Lucas Dantas <lucas.dantas@ifrn.edu.br>
2024-05-07 01:28:48 -0300

    Revisão de código e correção de bug no POST

--- toys/serializers.py
+++ toys/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from toys.models import Toy
 
+
 class ToySerializer(serializers.Serializer):
     pk = serializers.IntegerField(read_only=True)
     nome = serializers.CharField(max_length=150)
@@ -9,14 +10,14 @@ class ToySerializer(serializers.Serializer):
     categoria = serializers.CharField(max_length=200)
     foi_vendido = serializers.BooleanField(required=False)
 
-def create(self, validated_data):
-    return Toy.objects.create(**validated_data)
+    def create(self, validated_data):
+        return Toy.objects.create(**validated_data)
 
-def update(self, instance, validated_data):
-    instance.nome = validated_data.get('nome', instance.nome)
-    instance.descricao = validated_data.get('descricao', instance.descricao)
-    instance.lancamento = validated_data.get('lancamento', instance.lancamento)
-    instance.categoria = validated_data.get('categoria', instance.categoria)
-    instance.foi_vendido = validated_data.get('foi_vendido', instance.foi_vendido)
-    instance.save()
-    return instance
+    def update(self, instance, validated_data):
+        instance.nome = validated_data.get("nome", instance.nome)
+        instance.descricao = validated_data.get("descricao", instance.descricao)
+        instance.lancamento = validated_data.get("lancamento", instance.lancamento)
+        instance.categoria = validated_data.get("categoria", instance.categoria)
+        instance.foi_vendido = validated_data.get("foi_vendido", instance.foi_vendido)
+        instance.save()
+        return instance
--- toys/urls.py
+++ toys/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from toys import views
 
 urlpatterns = [
-    path('list/',views.toy_list),
-    path('details/<int:pk>/', views.toy_detail),
+    path("list/", views.toy_list),
+    path("details/<int:pk>/", views.toy_detail),
 ]
